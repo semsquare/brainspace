f00d72e394b0e3b9c53ae7b9db77d8ad7cd30220
Sambath Piazza <s.piazza@brainspace.it>
2022-03-14 16:05:28 +0100

    Initial Commit

---
+++ app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Sambath Piazza on 14/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
